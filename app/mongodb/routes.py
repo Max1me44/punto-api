46eb3e33034d2f52257abec8fefc7aee3d7a27d7
Maxime Chargé <mamaxdu44470@gmail.com>
2023-11-30 04:39:49 +0100

    Modif route pour récupérer informations sur un joueur

--- app/mongodb/routes.py
+++ app/mongodb/routes.py
@@ -1,6 +1,4 @@
 from fastapi import APIRouter, HTTPException
-from fastapi.encoders import jsonable_encoder
-
 from . import crud
 
 router = APIRouter()
@@ -26,13 +24,19 @@ def create_partie(nom: str, gagnant: bool):
 
 
 @router.get("/joueurs/{nom}/")
-def get_joueur_by_nom(nom: str):
+def get_joueur_info(nom: str):
     joueur = crud.get_joueur_by_nom(nom=nom)
     if joueur is None:
         raise HTTPException(status_code=404, detail="Joueur non trouvé")
+    nombre_parties = crud.get_nombre_parties_joueur(id_joueur=joueur["_id"])
+    nombre_parties_gagnees = crud.get_nombre_parties_gagnees_joueur(id_joueur=joueur["_id"])
     # Convertir l'ObjectId en str avant de le retourner
     joueur['_id'] = str(joueur['_id'])
-    return jsonable_encoder(joueur)
+    return {
+        "nom": joueur["nom"],
+        "nombre_parties": nombre_parties,
+        "nombre_parties_gagnees": nombre_parties_gagnees
+    }
 
 
 @router.get("/joueurs/{nom}/nombre_parties/")
--- app/sqlite/routes.py
+++ app/sqlite/routes.py
@@ -1,5 +1,4 @@
 from fastapi import APIRouter, Depends, HTTPException
-from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 from . import crud, database
 
@@ -26,11 +25,17 @@ def create_partie(nom_joueur: str, gagnant: bool, db: Session = Depends(database
 
 
 @router.get("/joueurs/{nom}/")
-def get_joueur_by_nom(nom: str, db: Session = Depends(database.get_db)):
-    db_joueur = crud.get_joueur_by_nom(db=db, nom=nom)
-    if db_joueur is None:
+def get_joueur_info(nom: str, db: Session = Depends(database.get_db)):
+    joueur = crud.get_joueur_by_nom(db=db, nom=nom)
+    if joueur is None:
         raise HTTPException(status_code=404, detail="Joueur non trouvé")
-    return jsonable_encoder(db_joueur)
+    nombre_parties = crud.get_nombre_parties_joueur(db=db, id_joueur=joueur.id)
+    nombre_parties_gagnees = crud.get_nombre_parties_gagnees_joueur(db=db, id_joueur=joueur.id)
+    return {
+        "nom": joueur.nom,
+        "nombre_parties": nombre_parties,
+        "nombre_parties_gagnees": nombre_parties_gagnees
+    }
 
 
 @router.get("/joueurs/{nom}/nombre_parties/")
